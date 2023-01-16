ce8d2caba4404f05bbe15f6d21b93c88e6026da6
@luongchihao2143 <hao.luong@hdwebsoft.dev>
2023-01-17 00:08:46 +0700

    initial

---
+++ src/assets/img/index.ts
@@ -0,0 +1,3 @@
+export const images = {
+  walletLogo: require('./wallet_logo.png'),
+};
--- src/screens/LoginScreen/index.tsx
+++ src/screens/LoginScreen/index.tsx
@@ -1,13 +1,21 @@
 import React from 'react';
 import AntDesign from 'react-native-vector-icons/AntDesign';
 import {CustomText, RootView, View} from '../../Components';
+import {colors} from '../../Theme';
 
 const LoginScreen = () => {
   return (
     <RootView p={16}>
-      <View flexDirection="row">
-        <AntDesign name="wallet" size={24} />
-        <CustomText ml={12} text="Money Saving" />
+      <View alignItems="center" flexDirection="row">
+        <AntDesign color={colors.green} name="wallet" size={24} />
+        <CustomText
+          ml={12}
+          color={colors.green}
+          text="Money Saving"
+          size={18}
+          lineHeight={20}
+          fontWeight="bold"
+        />
       </View>
     </RootView>
   );
